0527bad8c47b9b00bd79d1350df4730262d0dd95
JS-Develope <jeanaparecido21@gmail.com>
2024-10-19 17:41:55 -0300

    feat: criando estrutura e form para envio de mensagem para validação

--- aplicativo/gerenciador_mensagens/lib/pages/message/message.dart
+++ aplicativo/gerenciador_mensagens/lib/pages/message/message.dart
@@ -1,13 +1,81 @@
 import 'package:flutter/material.dart';
 
-class Message extends StatelessWidget {
+class Message extends StatefulWidget {
   const Message({super.key});
+
+  @override
+  State<Message> createState() => _MessageState();
+}
+
+class _MessageState extends State<Message> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Mensagem')),
-      body: const Center(child: Text('Mensagem')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _messageController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                  ),
+                  labelText: 'Informe o SMS recebido',
+                  contentPadding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0)
+                ),
+                maxLines: null,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, insira o SMS';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print(_messageController.text);
+                        _messageController.clear();
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                    ),
+                    child: const Text('Enviar', style: TextStyle(color: Colors.white),),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
   }
 }
