0d36ed0dc05cc5877c49ea339aeef9eba4bd6dfb
jeanunicesumar <124851594+jeanunicesumar@users.noreply.github.com>
2024-10-20 09:54:53 -0300

    feat: criando tela com switch buttons (#6)
    Co-authored-by: JS-Develope <jeanaparecido21@gmail.com>

--- aplicativo/gerenciador_mensagens/lib/pages/config/config.dart
+++ aplicativo/gerenciador_mensagens/lib/pages/config/config.dart
@@ -1,14 +1,62 @@
-
 import 'package:flutter/material.dart';
 
-class Config extends StatelessWidget {
+class Config extends StatefulWidget {
   const Config({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ConfigState();
+}
+
+class _ConfigState extends State<Config> {
+  
+  bool monitorBackground = false;
+  bool saveInList = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Configuração')),
-      body: const Center(child: Text('Configuração')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const Text("Salvar mensagens em lista"),
+                  const Spacer(),
+                  Switch(
+                    value: saveInList,
+                    onChanged: (value) {
+                      setState(() {
+                        saveInList = value;
+                      });
+                    },
+                    activeTrackColor: Colors.lightBlue,
+                    activeColor: Colors.lightBlue[50],
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  const Text("Monitorar mensagens em segundo plano"),
+                  const Spacer(),
+                  Switch(
+                    value: monitorBackground,
+                    onChanged: (value) {
+                      setState(() {
+                        monitorBackground = value;
+                      });
+                    },
+                    activeTrackColor: Colors.lightBlue,
+                    activeColor: Colors.lightBlue[50],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
