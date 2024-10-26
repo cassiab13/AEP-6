be389fcce7bf5a0598eac296e210544f1375d6bb
Cassia <125088421+cassiab13@users.noreply.github.com>
2024-10-26 20:20:38 -0300

    011-feat-criacao-endpoint (#9)

--- aplicativo/gerenciador_mensagens/lib/model/validation.dart
+++ aplicativo/gerenciador_mensagens/lib/model/validation.dart
@@ -2,25 +2,25 @@
 class Validation {
   int _id;
   String _message;
-  bool _isValid = true;
+  bool _isSafe = true;
   DateTime _date;
   String _cause;
 
-  Validation._internal(this._id, this._message, this._isValid, this._date, this._cause);
+  Validation._internal(this._id, this._message, this._isSafe, this._date, this._cause);
 
   int get id => _id;
   String get message => _message;
-  bool get isValid => _isValid;
+  bool get isSafe => _isSafe;
   DateTime get date => _date;
   String get cause => _cause;
 
   factory Validation(Map<String, dynamic> body) {
     int id = body['id'];
     String message = body['message'];
-    bool isValid = body['isValid'];
+    bool isSafe = body['isSafe'];
     DateTime date = DateTime.parse(body['date']);
     String cause = body['cause'];
 
-    return Validation._internal(id, message, isValid, date, cause);
+    return Validation._internal(id, message, isSafe, date, cause);
   }
 }
--- aplicativo/gerenciador_mensagens/lib/pages/message/message.dart
+++ aplicativo/gerenciador_mensagens/lib/pages/message/message.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciador_mensagens/model/validation.dart';
+import 'package:gerenciador_mensagens/requests/validation_request.dart';
 
 class Message extends StatefulWidget {
   const Message({super.key});
@@ -10,7 +12,8 @@ class Message extends StatefulWidget {
 class _MessageState extends State<Message> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _messageController = TextEditingController();
-
+  final ValidationRequest validationRequest = ValidationRequest(
+      baseUrl: 'http://127.0.0.1:8000', endpoint: '/url_checker/analyze');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,18 +27,18 @@ class _MessageState extends State<Message> {
               TextFormField(
                 controller: _messageController,
                 decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 1.5),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 1.5),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.blue, width: 1.5),
-                  ),
-                  labelText: 'Informe o SMS recebido',
-                  contentPadding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0)
-                ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 1.5),
+                    ),
+                    labelText: 'Informe o SMS recebido',
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0)),
                 maxLines: null,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
@@ -49,10 +52,11 @@ class _MessageState extends State<Message> {
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   ElevatedButton(
-                    onPressed: () {
+                    onPressed: () async {
                       if (_formKey.currentState!.validate()) {
-                        print(_messageController.text);
-                        _messageController.clear();
+                        var results = await validationRequest
+                            .analyzeMessage(_messageController.text);
+                        _showResultsDialog(results);
                       }
                     },
                     style: ElevatedButton.styleFrom(
@@ -60,9 +64,13 @@ class _MessageState extends State<Message> {
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(0),
                       ),
-                      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12),
                     ),
-                    child: const Text('Enviar', style: TextStyle(color: Colors.white),),
+                    child: const Text(
+                      'Enviar',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 ],
               ),
@@ -70,6 +78,47 @@ class _MessageState extends State<Message> {
           ),
         ),
       ),
+    );
+  }
+
+  void _showResultsDialog(List<dynamic> results) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Resultado'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: results.map<Widget>((result) {
+                Validation validationResult = Validation({
+                  'id': 1,
+                  'message': _messageController.text,
+                  'isSafe': result['safe'],
+                  'date': DateTime.now().toIso8601String(),
+                  'cause': 'Nenhum problema encontrado'
+                });
+
+                String message = validationResult.isSafe
+                    ? 'URL Segura'
+                    : 'URL Insegura. Não clique!';
+
+                Color textColor =
+                    validationResult.isSafe ? Colors.green : Colors.red;
+
+                return Text(message, style: TextStyle(color: textColor, fontSize: 20));
+              }).toList(),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar'),
+            ),
+          ],
+        );
+      },
     );
   }
 
